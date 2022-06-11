f812a18542f4bc200a4215fc45f054d22aca9675
gustavo <gustavo.felipebc@gmail.com>
2022-06-11 17:26:48 -0300

    correção do bug do segundo, alterações no css

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import Play from '../../assets/play.svg';
-import Pause from '../../assets/pause.svg';
+import Play from '../../assets/play1.svg';
+import Pause from '../../assets/pause1.svg';
 
 import soundAlert from '../../assets/alert.mp3';
 
@@ -14,8 +14,10 @@ const Timer = () => {
   const [isPaused, setIsPaused] = useState(true);
   const [isActive, setIsActive] = useState(false);
 
+  let interval;
+
   useEffect(() => {
-    let interval = setInterval(() => {
+    interval = setInterval(() => {
       if (isActive && isPaused === false) {
         clearInterval(interval);
         if (seconds === 0) {
@@ -37,6 +39,7 @@ const Timer = () => {
           setSeconds(seconds - 1);
         }
       }
+      clearInterval(interval);
     }, 1000);
   }, [isActive, isPaused, seconds]);
 
@@ -55,9 +58,13 @@ const Timer = () => {
 
   function toggleColorStart() {
     document.querySelector('.circle').classList.toggle('active');
+    document.querySelector('.circle').classList.remove('pomodoro-active');
+    document.querySelector('.circle').classList.remove('short-break-active');
+    document.querySelector('.circle').classList.remove('long-break-active');
   }
 
   function handlePause() {
+    clearInterval(interval);
     setIsPaused(!isPaused);
     toggleColorPause();
   }
@@ -66,40 +73,120 @@ const Timer = () => {
     document.querySelector('.circle').classList.toggle('paused');
   }
 
+  function toggleColorPomodoro() {
+    document.querySelector('.circle').classList.add('pomodoro-active');
+    document.querySelector('.circle').classList.remove('short-break-active');
+    document.querySelector('.circle').classList.remove('long-break-active');
+  }
+
+  function toggleColorShortBreak() {
+    document.querySelector('.circle').classList.remove('pomodoro-active');
+    document.querySelector('.circle').classList.add('short-break-active');
+    document.querySelector('.circle').classList.remove('long-break-active');
+  }
+
+  function toggleColorLongBreak() {
+    document.querySelector('.circle').classList.remove('pomodoro-active');
+    document.querySelector('.circle').classList.remove('short-break-active');
+    document.querySelector('.circle').classList.add('long-break-active');
+  }
+
   function alertSound() {
     document.getElementById('audio').play();
     setTimeout(() => {
-      alert('Pomodoro concluído!');
+      alert('Ciclo concluído!');
     }, 100);
   }
 
-  return (
-    <section className="circle">
-      <div className="timer">
-        <audio src={soundAlert} id="audio"></audio>
-        {timerMinutes}
-        <span>:</span>
-        {timerSeconds}
-      </div>
-      {!isActive && (
-        <button onClick={handleStart}>
-          Iniciar
-          {/* <img src={Play} alt="iniciar" /> */}
-        </button>
-      )}
-      {isPaused === false && (
-        <button onClick={handlePause}>
-          <img src={Pause} alt="pausar" />
-        </button>
-      )}
+  function handleStartPomodoro() {
+    clearInterval(interval);
+    document.getElementById('cicle').innerText = 'Pomodoro';
+    document.getElementById('cicle').style = 'color: var(--red)';
+    toggleColorPomodoro();
+    setIsActive(false);
+    setIsPaused(true);
+    setMinutes(25);
+    setSeconds(0);
+  }
 
-      {isPaused === true && isActive === true ? (
-        <button onClick={handlePause}>
-          <img src={Play} alt="retomar" />
+  function handleStartShortBreak() {
+    clearInterval(interval);
+    document.getElementById('cicle').innerText = 'Short Break';
+    document.getElementById('cicle').style = 'color: var(--blue)';
+    toggleColorShortBreak();
+    setIsActive(false);
+    setIsPaused(true);
+    setMinutes(5);
+    setSeconds(0);
+  }
+
+  function handleStartLongBreak() {
+    clearInterval(interval);
+    document.getElementById('cicle').innerText = 'Long Break';
+    document.getElementById('cicle').style = 'color: var(--purpure)';
+    toggleColorLongBreak();
+    setMinutes(15);
+    setSeconds(0);
+  }
+
+  return (
+    <section>
+      <div className="container-controls">
+        <button
+          onClick={handleStartPomodoro}
+          className="function pomodoro-button"
+        >
+          Pomodoro
         </button>
-      ) : (
-        ''
-      )}
+        <button
+          onClick={handleStartShortBreak}
+          className="function short-break-button"
+        >
+          Short break
+        </button>
+        <button
+          onClick={handleStartLongBreak}
+          className="function long-break-button"
+        >
+          Long break
+        </button>
+      </div>
+
+      <div className="circle">
+        <div className="timer">
+          <audio src={soundAlert} id="audio"></audio>
+          {timerMinutes}
+          <span>:</span>
+          {timerSeconds}
+        </div>
+
+        {!isActive && (
+          <button className="button-action" onClick={handleStart}>
+            Iniciar
+            {/* <img src={Play} alt="iniciar" /> */}
+          </button>
+        )}
+        {isPaused === false && (
+          <button className="button-action" onClick={handlePause}>
+            <img src={Pause} alt="pausar" />
+          </button>
+        )}
+
+        {isPaused === true && isActive === true ? (
+          <button className="button-action" onClick={handlePause}>
+            <img src={Play} alt="retomar" />
+          </button>
+        ) : (
+          ''
+        )}
+      </div>
+      <h2
+        id="cicle"
+        className="cicle"
+        style={{ color: 'var(--background-circle)' }}
+      >
+        Bem vindo ao pomos!
+      </h2>
     </section>
   );
 };
